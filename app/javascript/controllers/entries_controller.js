9f82abdde009819afa7e82d04e9880c0ea7a52b5
Jean-Francis Bastien <bhacaz@gmail.com>
2021-03-01 08:00:41 -0500

    save

--- app/javascript/controllers/entries_controller.js
+++ app/javascript/controllers/entries_controller.js
@@ -2,8 +2,13 @@ import { Controller } from "stimulus"
 import bulmaCalendar from "bulma-extensions/bulma-calendar/dist/js/bulma-calendar.min.js"
 
 export default class extends Controller {
+  static targets = ['soldSum']
+
   resetForm() {
     this.element.reset()
+    console.log(this.element)
+    console.log(this.soldSumTarget)
+    this.soldSumTarget.innerHTML = 'Hello';
   }
 
   connect() {
